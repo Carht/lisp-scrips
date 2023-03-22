7097ac907f78126a329bc319825955c8086951d6
Carht <carht@protonmail.com>
2023-03-22 15:12:36 -0400

    #1 Resolv the only file input and best make up output.

--- list-big-files.lisp
+++ list-big-files.lisp
@@ -1,12 +1,18 @@
 (ql:quickload '(:osicat :uiop :cl-fad))
 
 (defparameter *dir* nil
-  "All the directories")
+  "All file paths")
 
 (defun size-file (filepath)
   "For some `filepath' return the size."
   (let ((stat (osicat-posix:stat filepath)))
     (osicat-posix:stat-size stat)))
+
+(defun directory-p (filepath)
+  "Is a directory?"
+  (if (cl-fad:directory-exists-p filepath)
+      t
+      nil))
 
 (defun walker (filepath)
   "List the files and directories of a directory, recursively."
@@ -15,8 +21,15 @@
 			   (push (list (format nil "~A" file) (size-file file)) *dir*))
 			 :directories t))
 
-(defun list-major-files (filepath)
-  "Return a list of list files and filesize."
-  (progn
-    (walker filepath)
-    (sort *dir* #'> :key #'second)))
+(defun list-big-files (filepath)
+  "Check if the input is a file or a directory, return the list of files ordered by size."
+  (if (directory-p filepath)
+      (progn
+	(setf *dir* nil)
+	(walker filepath)
+	(let ((sorted-filepaths (sort *dir* #'> :key #'second)))
+	  (mapcar #'(lambda (register)
+		      (format nil "~A" (concatenate 'string (first register) " "
+						     (write-to-string (second register)))))
+		  sorted-filepaths)))
+      (format nil "~A ~A" filepath (size-file filepath))))
